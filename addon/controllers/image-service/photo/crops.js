eacd6854bfa8d75f576bc444b7edf173802e8a4a
David Saenz Tagarro <david.saenz@hola-internet.com>
2015-09-25 13:56:08 +0200

    Fixed croptypes

--- addon/controllers/image-service/photo/crops.js
+++ addon/controllers/image-service/photo/crops.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.ObjectController.extend({
   cropsService: Ember.inject.service('image-service.crops'),
   photosController: Ember.inject.controller('image-service.photos'),
-  photo: Ember.computed.reads('photosController.photo'),
-  croptypes: Ember.computed.reads('model.croptypes'),
+  photo: Ember.computed.reads('photoController.photo'),
+  croptypes: Ember.computed.reads('photoController.croptypes'),
   actions: {
     addCrops() {
       var cropsService = this.get('cropsService');
