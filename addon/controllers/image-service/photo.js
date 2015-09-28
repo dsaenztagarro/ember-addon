c68147e26aa4631d8876334e9bbd81505a2f716e
David Saenz Tagarro <david.saenz@hola-internet.com>
2015-09-28 18:08:24 +0200

    Added scopeable final solution

--- addon/controllers/image-service/photo.js
+++ addon/controllers/image-service/photo.js
@@ -1,12 +1,42 @@
 import Ember from 'ember';
 
+var Scope = Ember.Object.extend({
+  init() {
+    if (this.get('controller') === undefined) {
+      throw 'MandatoryAttributeError: controller';
+    }
+    if (this.get('namespace') === undefined) {
+      throw 'MandatoryAttributeError: namespace';
+    }
+  },
+  transitionToRoute(relativeRouteName) {
+    var absoluteRouteName = this.get('namespace') + '.' + relativeRouteName;
+    debugger
+    return this.get('controller').transitionToRoute(absoluteRouteName);
+  }
+});
+
+// TODO: Scopeable
 export default Ember.Controller.extend({
   photos: Ember.computed.reads('model.photos'),
   photo: Ember.computed.reads('model.photo'),
+  init() {
+    this.initScope();
+    if (typeof this.post_init === 'function') {
+      this.post_init();
+    }
+  },
+  initScope() {
+    var scope = Scope.create({ namespace: 'image-service', controller: this });
+    this.set('localScope', scope);
+  },
+  scope() {
+    return this.get('localScope');
+  },
   actions: {
     selectPhoto(photo) {
       this.get('localCacheService').add('photo', photo);
-      this.transitionToRoute('image-service.photo.properties', photo.id);
+      this.scope().transitionToRoute('photo.properties', { photo_id: photo.id });
     }
   }
 });
--- addon/controllers/image-service/photo/properties.js
+++ addon/controllers/image-service/photo/properties.js
@@ -1,13 +1,44 @@
 import Ember from 'ember';
 // import PhotoTabMixin from 'ember-image-service/mixins/tabs/photo';
 
+var Scope = Ember.Object.extend({
+  init() {
+    if (this.get('controller') === undefined) {
+      throw 'MandatoryAttributeError: controller';
+    }
+    if (this.get('namespace') === undefined) {
+      throw 'MandatoryAttributeError: namespace';
+    }
+  },
+  transitionToRoute(relativeRouteName) {
+    var absoluteRouteName = this.get('namespace') + '.' + relativeRouteName;
+    debugger
+    return this.get('controller').transitionToRoute(absoluteRouteName);
+  }
+});
+
 export default Ember.ObjectController.extend({
   photoController: Ember.inject.controller('photo'),
   photo: Ember.computed.reads('photoController.photo'),
+  init() {
+    this.initScope();
+    if (typeof this.post_init === 'function') {
+      this.post_init();
+    }
+  },
+  initScope() {
+    var scope = Scope.create({ namespace: 'image-service', controller: this });
+    this.set('localScope', scope);
+  },
+  scope() {
+    debugger
+    return this.get('localScope');
+  },
   actions: {
     selectImage(photo) {
       this.get('cacheService').add('photo', photo);
-      this.transitionToRoute('image-service.photo.properties', { photo_id: photo.id });
+      // this.transitionToRoute('image-service.photo.properties', { photo_id: photo.id });
+      this.scope().transitionToRoute('photo.properties', { photo_id: photo.id });
     }
   }
 });
