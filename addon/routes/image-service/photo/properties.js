2d5fc22cf7fef0e430855611f5c3ba95b1e9f05e
David Saenz Tagarro <david.saenz@hola-internet.com>
2015-09-28 09:59:02 +0200

    Removed dead code

--- addon/routes/image-service/photo/properties.js
+++ addon/routes/image-service/photo/properties.js
@@ -2,10 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   photosService: Ember.inject.service('image-service.photos'),
-
-  model: function() {
-    return {
-      photo: this.get('localCacheService').get('photo') /* current photo selected on list */
-    };
-  },
 });
