37fdec57c6733fc109deb5e76fd16ea3f2717f95
David Saenz Tagarro <david.saenz@hola-internet.com>
2015-09-29 10:44:28 +0200

    Fixed routing wrapper

--- addon/controllers/image-service/photo.js
+++ addon/controllers/image-service/photo.js
@@ -1,67 +1,14 @@
 import Ember from 'ember';
-// import RoutingWrapper from 'ember-core/wrappers/routing/controller-wrapper';
+import ApplicationController from 'ember-image-service/controllers/application';
 
-var RoutingWrapper = Ember.Object.extend({
-  init() {
-    if (this.get('controller') === undefined) {
-      throw 'MandatoryAttributeError: controller';
-    }
-    if (this.get('namespace') === undefined) {
-      throw 'MandatoryAttributeError: namespace';
-    }
-  },
-  /**
-   * Transition the application into another route in the scope of the client
-   * application.
-   *
-   * @method transitionToRoute
-   * @public
-   */
-  transitionToRoute() {
-    var ctrl = this.get('controller');
-    var args = Array.prototype.slice.call(arguments);
-    var routeName = this.get('namespace') + '.' + args.shift();
-    args.splice(0, 0, routeName);
-    debugger
-    // return ctrl.transitionToRoute.apply(ctrl, [args[0]]);
-    return this.get('controller').transitionToRoute(args[0]);
-  }
-});
-
-
-// TODO: Scopeable
-export default Ember.Controller.extend({
+export default ApplicationController.extend({
   photos: Ember.computed.reads('model.photos'),
   photo: Ember.computed.reads('model.photo'),
-  init() {
-    var routingWrapper = RoutingWrapper.create({
-      namespace: 'image-service',
-      controller: this
-    });
-    this.set('routingWrapper', routingWrapper);
-  },
-  /**
-   *
-   *
-   * @method methodName
-   * @param {String} foo Argument 1
-   * @param {Object} config A config object
-   * @param {String} config.name The name on the config object
-   * @param {Function} config.callback A callback function on the config object
-   * @param {Boolean} [extra=false] Do extra, optional work
-   * @return {Boolean} Returns true on success
-   */
-  client() {
-    return this.get('routingWrapper');
-  },
 
   actions: {
     selectPhoto(photo) {
       this.get('localCacheService').add('photo', photo);
-      // this.client().transitionToRoute('photo.properties', { photo_id: photo.id });
-      debugger
-      // this.transitionToRoute('image-service.photo.properties', { photo_id: photo.id });
-      this.transitionToRoute('image-service.photo.properties', { photo_id: photo.id });
+      this.client().transitionToRoute('photo.properties', photo.id);
     }
   }
 });
--- addon/controllers/image-service/photo/properties.js
+++ addon/controllers/image-service/photo/properties.js
@@ -1,22 +1,6 @@
 import Ember from 'ember';
 // import PhotoTabMixin from 'ember-image-service/mixins/tabs/photo';
 
-var Scope = Ember.Object.extend({
-  init() {
-    if (this.get('controller') === undefined) {
-      throw 'MandatoryAttributeError: controller';
-    }
-    if (this.get('namespace') === undefined) {
-      throw 'MandatoryAttributeError: namespace';
-    }
-  },
-  transitionToRoute(relativeRouteName) {
-    var absoluteRouteName = this.get('namespace') + '.' + relativeRouteName;
-    debugger
-    return this.get('controller').transitionToRoute(absoluteRouteName);
-  }
-});
-
 export default Ember.ObjectController.extend({
   photoController: Ember.inject.controller('photo'),
   photo: Ember.computed.reads('photoController.photo'),
@@ -33,6 +17,7 @@ export default Ember.ObjectController.extend({
   scope() {
     return this.get('localScope');
   },
+  /*
   actions: {
     selectImage(photo) {
       this.get('cacheService').add('photo', photo);
@@ -40,4 +25,5 @@ export default Ember.ObjectController.extend({
       this.scope().transitionToRoute('photo.properties', { photo_id: photo.id });
     }
   }
+  */
 });
