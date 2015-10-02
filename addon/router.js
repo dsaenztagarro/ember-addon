0ca34b2d08c73488baac5e26336d09e6f44acec0
David Saenz Tagarro <david.saenz@hola-internet.com>
2015-10-02 11:52:50 +0200

    Added repository config

--- addon/router.js
+++ addon/router.js
@@ -1,10 +1,10 @@
 export default function(router) {
   router.route('image-service', function() {
     this.route('photos');
-		this.route('photo', { path: '/photo/:photo_id' }, function() {
-        this.route('properties');
-        this.route('crops');
-		});
+    this.route('photo', { path: '/photo/:photo_id' }, function() {
+      this.route('properties');
+      this.route('crops');
+    });
     this.route('search');
     this.route('upload');
   });
